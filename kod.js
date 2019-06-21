e5e1cc2460fc8540595c6342f297126798d37968
RaulSZ <51963372+RaulSZ@users.noreply.github.com>
2019-06-21 02:52:58 -0700

    version 2

--- kod.js
+++ kod.js
@@ -47,3 +47,45 @@ var result=displayText.substr(0,l-1);
 display.innerHTML=result;
 
 }
+
+var resultn;
+
+function saveN(){
+
+var display=document.getElementById('display');
+
+var displayText=display.innerHTML;
+
+var array=displayText.split("= ");
+
+
+var result=array[1];
+
+resultn=result;
+
+}
+
+function returnN(){
+
+var display=document.getElementById('display');
+
+var displayText=display.innerHTML;
+
+var result=displayText+resultn;
+
+display.innerHTML=result;
+
+}
+
+function squareRoot(){
+
+var display=document.getElementById('display');
+
+var displayText=display.innerHTML;
+
+var s=Math.sqrt(displayText);
+
+display.innerHTML=s;
+
+}
+
